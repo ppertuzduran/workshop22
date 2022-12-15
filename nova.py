96984ccfa61f70628807c6b4d246ffcbc089d4e0
Pablo Andres Pertuz Duran <71804912+ppertuzduran@users.noreply.github.com>
2022-12-15 16:55:21 -0500

    Update nova.py

--- nova.py
+++ nova.py
@@ -7,16 +7,18 @@ server_parameter = '/servers'
 
 #NOVA SERVICE
 
-def nova_service(factor, headers, server_specs):
+def nova_service(factor, headers):
 
     if factor == 'flavors':
         nova_response = requests.get(url=f"{env_url}:{nova_port}/flavors", headers=headers).json().get('flavors')
 
     if factor == 'key_pairs':
         nova_response = requests.get(url=f"{env_url}:{nova_port}/os-keypairs", headers=headers).json().get('keypairs')
-
-    if factor == 'server':
-        nova_response = requests.post(url=f"{env_url}:{nova_port}/servers", headers=headers, json=server_specs)
-
+        
     return nova_response
+
+def nova_server_service(headers, server_specs):
+    nova_server_response = requests.post(url=f"{env_url}:{nova_port}/servers", headers=headers, json=server_specs)
+
+    return nova_server_response
 
